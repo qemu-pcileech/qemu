c599cc6c81d1c3a6be40a203c60b1139cadfe3e7
Zero Tang <zero.tangptr@gmail.com>
2024-09-03 10:28:21 +0800

    Reimplement PCILeech with chardev backend
    This commit only implements DMA read.
    Special thanks to Peter Maydell's review.

--- hw/misc/pcileech.c
+++ hw/misc/pcileech.c
@@ -33,23 +33,26 @@
 #include "qom/object.h"
 #include "qemu/main-loop.h" /* iothread mutex */
 #include "qemu/module.h"
+#include "chardev/char-fe.h"
 #include "qapi/visitor.h"
 
 #define TYPE_PCILEECH_DEVICE "pcileech"
 
+#define PCILEECH_REQUEST_READ   0
+#define PCILEECH_REQUEST_WRITE  1
+
 struct LeechRequestHeader {
-    uint8_t endianness; /* 0 - Little, 1 - Big */
     uint8_t command;    /* 0 - Read, 1 - Write */
-    uint8_t reserved[6];
-    /* Variable Endianness */
+    uint8_t reserved[7];
+    /* Little-Endian */
     uint64_t address;
     uint64_t length;
 };
 
 struct LeechResponseHeader {
-    uint8_t endianness; /* 0 - Little, 1 - Big */
-    uint8_t reserved[3];
-    MemTxResult result;
+    /* Little-Endian */
+    uint32_t result;
+    uint8_t reserved[4];
     uint64_t length;    /* Indicates length of data followed by header */
 };
 
@@ -60,14 +63,10 @@ static_assert(sizeof(struct LeechResponseHeader) == 16);
 struct PciLeechState {
     /* Internal State */
     PCIDevice device;
-    QemuThread thread;
-    QemuMutex mutex;
-    bool endianness;
-    bool stopping;
+    struct LeechRequestHeader request;
+    int pos;
     /* Communication */
-    char *host;
-    uint16_t port;
-    int sockfd;
+    CharBackend chardev;
 };
 
 typedef struct LeechRequestHeader LeechRequestHeader;
@@ -75,9 +74,9 @@ typedef struct PciLeechState PciLeechState;
 
 DECLARE_INSTANCE_CHECKER(PciLeechState, PCILEECH, TYPE_PCILEECH_DEVICE)
 
+/*
 static void pci_leech_process_write_request(PciLeechState *state,
-                                            LeechRequestHeader *request,
-                                            int incoming)
+                                            LeechRequestHeader *request)
 {
     char buff[1024];
     for (uint64_t i = 0; i < request->length; i += sizeof(buff)) {
@@ -103,155 +102,75 @@ static void pci_leech_process_write_request(PciLeechState *state,
         }
     }
 }
+*/
 
-static void pci_leech_process_read_request(PciLeechState *state,
-                                            LeechRequestHeader *request,
-                                            int incoming)
+static void pci_leech_process_read_request(PciLeechState *state)
 {
-    char buff[1024];
+    uint8_t buff[1024];
+    struct LeechRequestHeader *request = &state->request;
     for (uint64_t i = 0; i < request->length; i += sizeof(buff)) {
         struct LeechResponseHeader response = { 0 };
-        char* response_buffer = (char *)&response;
+        uint8_t * response_buffer = (uint8_t *)&response;
         const uint64_t readlen = (request->length - i) <= sizeof(buff) ?
                                     (request->length - i) : sizeof(buff);
         ssize_t sendlen = 0;
-        response.endianness = state->endianness;
-        response.result = pci_dma_read(&state->device, request->address + i,
+        MemTxResult result = pci_dma_read(&state->device, request->address + i,
                                                             buff, readlen);
-        if (response.result) {
+        if (result != MEMTX_OK) {
             printf("PCILeech: Address 0x%lX Read Error! MemTxResult: 0x%X\n",
-                    request->address + i, response.result);
+                    request->address + i, result);
         }
-        response.length = (request->endianness != state->endianness) ?
-                                            bswap64(readlen) : readlen;
+        response.result = cpu_to_le32(result);
+        response.length = cpu_to_le64(readlen);
         while (sendlen < sizeof(struct LeechResponseHeader)) {
-            sendlen += send(incoming, &response_buffer[sendlen],
-                            sizeof(struct LeechResponseHeader) - sendlen, 0);
+            sendlen += qemu_chr_fe_write_all(&state->chardev, &response_buffer[sendlen],
+                            sizeof(struct LeechResponseHeader) - sendlen);
         }
         sendlen = 0;
         while (sendlen < readlen) {
-            sendlen += send(incoming, &buff[sendlen], readlen - sendlen, 0);
+            sendlen += qemu_chr_fe_write_all(&state->chardev, &buff[sendlen], readlen - sendlen);
         }
     }
 }
 
-static void *pci_leech_worker_thread(void *opaque)
+static void pci_leech_chardev_read_handler(void* opaque, const uint8_t *buf, int size)
 {
-    PciLeechState *state = PCILEECH(opaque);
-    while (1) {
-        LeechRequestHeader request;
-        char *request_buffer = (char *)&request;
-        ssize_t received = 0;
-        int incoming;
-        struct sockaddr address;
-        socklen_t addrlen;
-        /* Check if we are stopping. */
-        qemu_mutex_lock(&state->mutex);
-        if (state->stopping) {
-            qemu_mutex_unlock(&state->mutex);
-            break;
+    PciLeechState *state=PCILEECH(opaque);
+    uint8_t* req_buff=(uint8_t*)&state->request;
+    if(state->pos+size<sizeof(struct LeechRequestHeader)) {
+        memcpy(&req_buff[state->pos],buf,size);
+        state->pos+=size;
+    } else {
+        memcpy(&req_buff[state->pos],buf,sizeof(struct LeechRequestHeader)-state->pos);
+        state->request.address=le64_to_cpu(state->request.address);
+        state->request.length=le64_to_cpu(state->request.length);
+        state->pos=0;
+        switch(state->request.command) {
+            case PCILEECH_REQUEST_READ:
+                pci_leech_process_read_request(state);
+                break;
+            case PCILEECH_REQUEST_WRITE:
+            default:
+                printf("PCILeech: unknown request command (%u) is received!\n",state->request.command);
+                break;
         }
-        qemu_mutex_unlock(&state->mutex);
-        /* Accept PCILeech requests. */
-        /* Use HTTP1.0-like protocol for simplicity. */
-        incoming = accept(state->sockfd, &address, &addrlen);
-        if (incoming < 0) {
-            puts("WARNING: Failed to accept socket for PCILeech! Skipping "
-                 "Request...\n");
-            continue;
-        }
-        /* Get PCILeech requests. */
-        while (received < sizeof(LeechRequestHeader)) {
-            received += recv(incoming, &request_buffer[received],
-                            sizeof(LeechRequestHeader) - received, 0);
-        }
-        /* Swap endianness. */
-        if (request.endianness != state->endianness) {
-            request.address = bswap64(request.address);
-            request.length = bswap64(request.length);
-        }
-        /* Process PCILeech requests. */
-        qemu_mutex_lock(&state->mutex);
-        if (request.command) {
-            pci_leech_process_write_request(state, &request, incoming);
-        } else {
-            pci_leech_process_read_request(state, &request, incoming);
-        }
-        qemu_mutex_unlock(&state->mutex);
-        close(incoming);
     }
-    return NULL;
+}
+
+static int pci_leech_chardev_can_read_handler(void* opaque)
+{
+    return sizeof(struct LeechRequestHeader);
 }
 
 static void pci_leech_realize(PCIDevice *pdev, Error **errp)
 {
     PciLeechState *state = PCILEECH(pdev);
-    struct sockaddr_in sock_addr;
-    char host_ip[16];
-    struct hostent *he = gethostbyname(state->host);
-    if (he == NULL) {
-        puts("gethostbyname failed!");
-        exit(EXIT_FAILURE);
-    }
-    /* Initialize the socket for PCILeech. */
-    state->sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (state->sockfd < 0) {
-        puts("Failed to initialize socket for PCILeech!");
-        exit(EXIT_FAILURE);
-    }
-    sock_addr.sin_family = AF_INET;
-    sock_addr.sin_addr = *(struct in_addr *)he->h_addr;
-    sock_addr.sin_port = htons(state->port);
-    inet_ntop(AF_INET, &sock_addr.sin_addr, host_ip, sizeof(host_ip));
-    if (bind(state->sockfd, (struct sockaddr *)&sock_addr, sizeof(sock_addr))
-                                                                    < 0) {
-        puts("Failed to bind socket for PCILeech!");
-        close(state->sockfd);
-        exit(EXIT_FAILURE);
-    }
-    if (listen(state->sockfd, 10) < 0) {
-        puts("Failed to listen to socket for PCILeech!");
-        close(state->sockfd);
-        exit(EXIT_FAILURE);
-    }
-    printf("INFO: PCILeech is listening on %s:%u...\n", host_ip, state->port);
-    /* Initialize the thread for PCILeech. */
-    qemu_mutex_init(&state->mutex);
-    qemu_thread_create(&state->thread, "pcileech", pci_leech_worker_thread,
-                                            state, QEMU_THREAD_JOINABLE);
-}
-
-static void pci_leech_finalize(PCIDevice *pdev)
-{
-    PciLeechState *state = PCILEECH(pdev);
-    puts("Stopping PCILeech Worker...");
-    qemu_mutex_lock(&state->mutex);
-    state->stopping = true;
-    qemu_mutex_unlock(&state->mutex);
-    close(state->sockfd);
-    qemu_thread_join(&state->thread);
-    qemu_mutex_destroy(&state->mutex);
-}
-
-char pci_leech_default_host[] = "0.0.0.0";
-
-static void pci_leech_instance_init(Object *obj)
-{
-    int x = 1;
-    char* y = (char *)&x;
-    PciLeechState *state = PCILEECH(obj);
-    /* QEMU's String-Property can't specify default value. */
-    /* So we have to set the default on our own. */
-    if (state->host == NULL) {
-        state->host = pci_leech_default_host;
-    }
-    /* Save Our Endianness. */
-    state->endianness = (*y == 0);
+    puts("PCILeech: Realize...");
+    qemu_chr_fe_set_handlers(&state->chardev,pci_leech_chardev_can_read_handler,pci_leech_chardev_read_handler,NULL,NULL,state,NULL,true);
 }
 
 static Property leech_properties[] = {
-    DEFINE_PROP_UINT16("port", PciLeechState, port, 6789),
-    DEFINE_PROP_STRING("host", PciLeechState, host),
+    DEFINE_PROP_CHR("chardev", PciLeechState, chardev),
     DEFINE_PROP_END_OF_LIST(),
 };
 
@@ -260,7 +179,6 @@ static void pci_leech_class_init(ObjectClass *class, void *data)
     DeviceClass *dc = DEVICE_CLASS(class);
     PCIDeviceClass *k = PCI_DEVICE_CLASS(class);
     k->realize = pci_leech_realize;
-    k->exit = pci_leech_finalize;
     /* Change the Vendor/Device ID to your favor. */
     /* These are the default values from PCILeech-FPGA. */
     k->vendor_id = PCI_VENDOR_ID_XILINX;
@@ -281,7 +199,6 @@ static void pci_leech_register_types(void)
         .name = TYPE_PCILEECH_DEVICE,
         .parent = TYPE_PCI_DEVICE,
         .instance_size = sizeof(PciLeechState),
-        .instance_init = pci_leech_instance_init,
         .class_init = pci_leech_class_init,
         .interfaces = interfaces,
     };
